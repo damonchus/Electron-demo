3f0b22b5f74bd3fd1be7f9edb487a844b00116a4
Damon Chu <damonchugs@foxmail.com>
2023-07-25 22:09:34 +0800

    删除敏感信息

--- src/renderer/src/appconfig.ts
+++ src/renderer/src/appconfig.ts
@@ -1,12 +1,6 @@
 export default {
   // 腾讯云
-  tenxunyun: {
-    FileMkdir: 'damonchu.fun/pic',
-    Bucket: 'damonchu-1301459681',
-    Region: 'ap-chengdu',
-    SecretId: 'AKIDCCwQdvJxe6pMgkm9ZjPq8AvQEn6wx094',
-    SecretKey: 'rWv5zJOEoAH8qIfngICqEMG2Z0B9rKXU'
-  },
+  tenxunyun: {},
   // 本地存储信息
   session: {
     RecordList: 'RecordList', // 游戏存档列表
